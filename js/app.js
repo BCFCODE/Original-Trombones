3e4d03ec708e47b5a3fce6ca182f9752775f5d67
Morteza Bakhshandeh <bcfcode@gmail.com>
2023-11-25 14:09:51 +0330

    some css styles added (footer, glasses and more)

---
+++ js/app.js
@@ -0,0 +1,4 @@
+const d = new Date()
+const year = d.getFullYear()
+const footerCopyright = document.getElementById('copyright')
+footerCopyright.textContent = `Copyright ${year}, BCFCODE - Designed by @Bakhshandeh_Morteza`;
